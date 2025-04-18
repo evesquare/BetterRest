4c086bbdd492936c8d7159851014a62b52676796
evesquare <50450074+EveSquare@users.noreply.github.com>
2025-04-18 20:26:31 +0900

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by evesquare on 2025/04/18.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
